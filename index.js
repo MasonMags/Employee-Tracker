b410b4fe1033edaab87049d2a582bd32912762b4
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-25 19:27:36 -0500

    viewing department table

--- index.js
+++ index.js
@@ -14,4 +14,13 @@ var employee_tracker = function () {
         name: 'prompt',
         message: 'What would you like to do?',
         choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
-    }])
+    }]).then((answers) => {
+        // Views the Department Table in the Database
+        if (answers.prompt === 'View All Department') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Departments: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } 
